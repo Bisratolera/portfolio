1a5f70331ab686a3996ab51869c6ad1fd7405b97
Bisratolera <leultolera@gmail.com>
2024-04-21 22:41:13 +0300

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div>
         <div className="flex flex-col md:flex-row p-4 md:p-28 w-full h-auto md:h-44 rounded-xl border-2 md:mt-44 justify-center">
-            <div className="list flex flex-row gap-20 items-center justify-center text-[#fff] text-4xl cursor-pointer ">
+            <div className="list flex flex-row gap-20 items-center justify-center text-[#ff1818] text-4xl cursor-pointer ">
                 <Fade direction="up"><FaTelegram /></Fade>
                 <Fade direction="up"><FaFacebook /></Fade>
                 <Fade direction="up"><FaWhatsapp /></Fade>
